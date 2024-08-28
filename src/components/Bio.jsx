7fbc5b5f1bfc18ef245f7d6bb86dfa40151b481d
Krit Verma <kverma@usc.edu>
2024-08-28 08:57:12 -0700

    Added Icon

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -29,7 +29,7 @@ function Bio() {
         <div>
           <h1 className='text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl'>Krit Verma</h1>
           <h2 className='mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl'>Backend Engineer</h2>
-          <p className="mt-4 max-w-xs leading-normal">I build </p>
+          {/* <p className="mt-4 max-w-xs leading-normal">I build </p> */}
           <nav className='nav hidden lg:block'>
             <ul className="mt-16 w-max space-y-4">
             <li>
